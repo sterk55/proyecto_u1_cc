eba08bbf762ceb73773f04d7ec797bced1e6a5b4
Jimmy Ortega <fabricioortega2011@gmail.com>
2022-12-07 19:47:02 -0500

    Taller9

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,16 @@
-function fdf(){
+function dividir(num1, num2){
+    return num1/num2
     
+}
+
+function sumar(num1, num2){
+    return num1+num2
+}
+
+function restar(num1, num2){
+    return num1-num2
+}
+
+function multiplicar(num1, num2){
+    return num1*num2
 }
