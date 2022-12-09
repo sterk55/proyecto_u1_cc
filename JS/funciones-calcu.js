17eeaca36a399d86984bf8d1dc5f4d7f11cadec0
sterk55 <daniel49571@gmail.com>
2022-12-09 00:01:13 -0500

    CalcuFinalizada

--- JS/funciones-calcu.js
+++ JS/funciones-calcu.js
@@ -1,27 +1,61 @@
 var valorDisplayTable1 = document.getElementById('idPrimeraTabla').style.display;
 var valorDisplayTable2 = document.getElementById('idSegundaTabla').style.display;
-var cursorValidoSumar = document.getElementById('idBotonSumar').style.cursor;
-var cursorValidoDividir = document.getElementById('idBotonDividir').style.cursor;
-var cursorValidoRestar = document.getElementById('idBotonRestar').style.cursor;
-var cursorValidoMultiplicar = document.getElementById('idBotonMultiplicar').style.cursor;
+var textoPrimerDigito = document.getElementById('idPrimerDigito').innerHTML;
+var textoSegundoDigito = document.getElementById('idSegundoDigito').innerHTML;
+var textoOperacion = document.getElementById('idOperacion').innerHTML;
+var textoResul = document.getElementById('resul').innerHTML;
 
 
-function dividir(num1, num2){
+function dividir(){
     valorDisplayTable1 = 'none';
     valorDisplayTable2 = 'block'
-    return num1/num2
     
+    document.getElementById('idPrimeraTabla').style.display = valorDisplayTable1;
+    document.getElementById('idSegundaTabla').style.display = valorDisplayTable2;
 }
 
-function sumar(num1, num2){
+function sumar(){
 
-    return num1+num2
+    valorDisplayTable1 = 'none';
+    valorDisplayTable2 = 'block'
+    
+    document.getElementById('idPrimeraTabla').style.display = valorDisplayTable1;
+    document.getElementById('idSegundaTabla').style.display = valorDisplayTable2;
 }
 
-function restar(num1, num2){
-    return num1-num2
+function restar(){
+    valorDisplayTable1 = 'none';
+    valorDisplayTable2 = 'block'
+    
+    document.getElementById('idPrimeraTabla').style.display = valorDisplayTable1;
+    document.getElementById('idSegundaTabla').style.display = valorDisplayTable2;
 }
 
-function multiplicar(num1, num2){
-    return num1*num2
+function multiplicar(){
+    valorDisplayTable1 = 'none';
+    valorDisplayTable2 = 'block'
+    
+    document.getElementById('idPrimeraTabla').style.display = valorDisplayTable1;
+    document.getElementById('idSegundaTabla').style.display = valorDisplayTable2;
+}
+
+function total(){
+    var textoOperacion = document.getElementById('idOperacion').innerHTML;
+    var textoResul = document.getElementById('resul').innerHTML;
+    var textoPrimerDigito = document.getElementById('idPrimerDigito').innerHTML;
+    var textoSegundoDigito = document.getElementById('idSegundoDigito').innerHTML;
+
+    if(textoOperacion == '+'){
+        textoResul = parseInt(textoPrimerDigito) + parseInt(textoSegundoDigito)
+        console.log("suma")
+    }else if(textoOperacion == '-'){
+        textoResul = parseInt(textoPrimerDigito) - parseInt(textoSegundoDigito) 
+    }else if(textoOperacion == '*'){
+        textoResul = parseInt(textoPrimerDigito) * parseInt(textoSegundoDigito) 
+    }else if(textoOperacion == '/'){
+        textoResul = parseInt(textoPrimerDigito) / parseInt(textoSegundoDigito) 
+    }
+
+    document.getElementById('idOperacion').innerHTML = textoOperacion
+    document.getElementById('resul').innerHTML = textoResul
 }
